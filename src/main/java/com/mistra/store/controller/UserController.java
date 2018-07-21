e78481c56547db23f53d0f6be8b37ac7210ed4f8
MistraR <mistra3030000>
2018-07-22 00:46:03 +0800

    login.js

--- src/main/java/com/mistra/store/controller/UserController.java
+++ src/main/java/com/mistra/store/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.mistra.store.controller;
 
 import com.mistra.store.entity.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Author: RoronoaZoro丶WangRui
  * Time: 2018/7/20/020
  * Describe:
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -20,4 +24,9 @@ public class UserController {
         return "index";
     }
 
+    @RequestMapping("/hello")
+    public String index(HttpServletRequest request, @RequestParam(value = "name", required = false, defaultValue = "springboot-thymeleaf") String name){
+        request.setAttribute("name", name);
+        return "hello";
+    }
 }
